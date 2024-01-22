12141386dbca55dc5e4f76a2ffedc7c982598364
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-22 15:26:07 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.miguel.workshopjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.miguel.workshopjavafx to javafx.fxml;
+    exports com.miguel.workshopjavafx;
+}
