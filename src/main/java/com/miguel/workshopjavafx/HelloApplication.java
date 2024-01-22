38b87c39d3c4ecfd7d36ec0cfa924bb87f491f3e
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-22 16:12:53 +0000

    Main view design
    Checklist:
     Design MainView.fxml
     Customize menu items
     Update Main.java

--- src/main/java/com/miguel/workshopjavafx/HelloApplication.java
+++ src/main/java/com/miguel/workshopjavafx/HelloApplication.java
@@ -2,19 +2,28 @@ package com.miguel.workshopjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+            ScrollPane scrollPane = loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToHeight(true);
+            Scene mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
