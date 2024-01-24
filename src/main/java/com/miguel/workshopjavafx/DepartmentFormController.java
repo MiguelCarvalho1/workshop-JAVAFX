6db83933701373604ed4244c342875e21a202b42
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-24 14:22:40 +0000

    Validation exception

--- src/main/java/com/miguel/workshopjavafx/DepartmentFormController.java
+++ src/main/java/com/miguel/workshopjavafx/DepartmentFormController.java
@@ -12,12 +12,11 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
     private Department entity;
@@ -57,8 +56,14 @@ public class DepartmentFormController implements Initializable {
             service.saveOrUpdate(entity);
             notifyDataChangeListeners();
             Utils.currentStage(event).close();
-        }catch (DbException e){
-            Alerts.showAlert("Erro saving object",null,e.getMessage(), Alert.AlertType.ERROR);
+
+        }catch (ValidationException e){
+
+        setErrorMessages(e.getErrors());
+
+        }catch (DbException e) {
+            
+            Alerts.showAlert("Erro saving object", null, e.getMessage(), Alert.AlertType.ERROR);
         }
     }
 
@@ -70,8 +75,16 @@ public class DepartmentFormController implements Initializable {
 
     private Department getFormData() {
         Department obj = new Department();
+        ValidationException exception = new ValidationException("Validation error");
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+        if(txtName.getText() == null || txtName.getText().trim().equals("")){
+            exception.addError("name", "Field can´t be empty");
+        }
         obj.setName(txtName.getText());
+
+        if(exception.getErrors().size() > 0){
+            throw exception;
+        }
         return obj;
     }
 
@@ -96,5 +109,12 @@ public class DepartmentFormController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+    }
+    public void setErrorMessages(Map<String, String> errors){
+        Set<String> fields = errors.keySet();
+
+        if(fields.contains("name")){
+            labelErrorName.setText(errors.get("name"));
+        }
     }
 }
